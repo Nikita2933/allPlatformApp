8a05b6508e3a9b30bb924332939a0632c6f8a7d2
nikita.ivanov <nikita.ivanov@btc-s.ru>
2022-07-01 11:08:56 +0300

    Initial Commit

---
+++ Shared/AllPlatformTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllPlatformTaskApp.swift
+//  Shared
+//
+//  Created by Никита Иванов on 01.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AllPlatformTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
